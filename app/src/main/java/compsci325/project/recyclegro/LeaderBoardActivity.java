041aadc56b960e0e8a73cdc781e7fc6c6bd22230
Daniel Zhang <44486997+zhangbdaniel@users.noreply.github.com>
2018-12-08 15:25:32 -0500

    added all the known view for the app

---
+++ app/src/main/java/compsci325/project/recyclegro/LeaderBoardActivity.java
@@ -0,0 +1,13 @@
+package compsci325.project.recyclegro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeaderBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader_board);
+    }
+}
