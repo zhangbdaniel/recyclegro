2e70cf8da4b286afefcbfa86efabd7a8a91e16b4
Daniel Zhang <44486997+zhangbdaniel@users.noreply.github.com>
2018-12-14 18:38:54 -0500

    update leaderboard view too new theme

--- app/src/main/java/compsci325/project/recyclegro/LeaderBoardActivity.java
+++ app/src/main/java/compsci325/project/recyclegro/LeaderBoardActivity.java
@@ -1,7 +1,13 @@
 package compsci325.project.recyclegro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class LeaderBoardActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class LeaderBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_leaderboard);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.leaderboard_menu, menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menu_leaderboards) {
+        Intent goToFriends = new Intent(this, FriendActivity.class);
+        startActivity(goToFriends);
+        return false;
     }
 }
