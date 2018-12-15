bac6c8ac65a079a614ec66449892bc06a33c0e80
Gaurab Pant <bpant@umass.edu>
2018-12-14 21:31:55 -0500

    Saved

--- app/src/main/java/compsci325/project/recyclegro/BarcodeReaderActivity.java
+++ app/src/main/java/compsci325/project/recyclegro/BarcodeReaderActivity.java
@@ -1,201 +1,45 @@
 package compsci325.project.recyclegro;
 
-import android.content.DialogInterface;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.graphics.Point;
-import android.graphics.Rect;
-import com.google.android.gms.tasks.OnFailureListener;
-import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import java.util.List;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import java.io.IOException;
 
-import com.google.zxing.Result;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+public class BarcodeReaderActivity extends AppCompatActivity {
 
-import static android.Manifest.permission_group.CAMERA;
-
-
-public class BarcodeReaderActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
-
+    SurfaceView surfaceView;
+    TextView txtBarcodeValue;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    Button btnAction;
+    String intentData = "";
+    boolean isEmail = false;
     private static final int REQUEST_CAMERA = 1;
-    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        scannerView = new ZXingScannerView(this);
-        setContentView(scannerView);
-
-        //if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        //{
-            if(checkPermission())
-            {
-            Toast.makeText(BarcodeReaderActivity.this, "Permission is granted!", Toast.LENGTH_LONG).show();
-            }
-            else
-            {
-                requestPermission();
-            }
-        //}
-
-
-
 
     }
-    //slighly different than video
-    private boolean checkPermission(){
-        return (ContextCompat.checkSelfPermission(BarcodeReaderActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED);
-    }
+}
 
-    private void requestPermission(){
-        ActivityCompat.requestPermissions(this,new String[]{CAMERA}, REQUEST_CAMERA);
-    }
-
-    public void onRequestPermissionResult(int requestCode, String permissions[], int[] grantResults )
-    {
-        switch(requestCode)
-        {
-            case REQUEST_CAMERA :
-                if(grantResults.length > 0)
-                {
-                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if(cameraAccepted)
-                    {
-                        Toast.makeText(BarcodeReaderActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(BarcodeReaderActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
-                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                        {
-                            if(shouldShowRequestPermissionRationale(CAMERA))
-                            {
-                              displayAlertMessage("You need to allow access for permissions", new DialogInterface.OnClickListener() {
-                                  @Override
-                                  public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
-                                  }
-                              });
-                              return;
-                            }
-                        }
-                    }
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
-            if(checkPermission())
-            {
-                if(scannerView == null)
-                {
-                    scannerView = new ZXingScannerView(this);
-                    setContentView(scannerView);
-                }
-                scannerView.setResultHandler(this);
-                scannerView.startCamera();
-            }
-            else
-            {
-                requestPermission();
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        scannerView.stopCamera();
-    }
-
-    public void displayAlertMessage(String message, DialogInterface.OnClickListener listner)
-    {
-    new AlertDialog.Builder(BarcodeReaderActivity.this)
-            .setMessage(message)
-            .setPositiveButton("OK", listner)
-            .setNegativeButton("Cancel",null)
-            .create()
-            .show();
-    }
-
-    @Override
-    public void handleResult(Result result){
-    final String scanResult = result.getText();
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Scan Result");
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    scannerView.resumeCameraPreview(BarcodeReaderActivity.this);
-                }
-            });
-            //if theres a link you can visit it with the Visit button, probably is not needed at all
-            builder.setNeutralButton("Visit", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(scanResult));
-                    startActivity(intent);
-                }
-            });
-            builder.setMessage(scanResult);
-            AlertDialog alert = builder.create();
-            alert.show();
-    }
-
-    private void scanBarcodes(FirebaseVisionImage image) {
-        FirebaseVisionBarcodeDetectorOptions options =
-                new FirebaseVisionBarcodeDetectorOptions.Builder()
-                        .setBarcodeFormats(
-                                FirebaseVisionBarcode.FORMAT_QR_CODE)
-                        .build();
-
-        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
-                .getVisionBarcodeDetector();
-
-        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
-                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
-                    @Override
-                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
-                        // Task completed successfully
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Task failed with an exception
-                    }
-                });
-    }
-
-
-
-    }
 
