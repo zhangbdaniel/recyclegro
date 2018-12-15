f0c187080459da9ac3449dbc4de6343558fcfa54
thubbard0 <thubbard@umass.edu>
2018-12-14 20:23:08 -0500

    Working on Adapter & Friends List

---
+++ app/src/main/java/compsci325/project/recyclegro/User.java
@@ -0,0 +1,27 @@
+package compsci325.project.recyclegro;
+
+public class User {
+    private String name;
+    private String score;
+
+    public User(String name, String score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
